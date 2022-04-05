25ee189b3a2aff7a2802f605cd641c9d22a6c8bf
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2022-04-05 14:53:51 +0200

    better "google sign in" look

--- src/components/GoogleSignIn.tsx
+++ src/components/GoogleSignIn.tsx
@@ -13,42 +13,38 @@ export const GoogleSignIn: React.FC<GoogleSignInProps> = ({
       style={{
         width: "240px",
       }}
-      className="mx-auto rounded-xl justify-center items-center text-white flex font-medium bg-blue-500 hover:bg-blue-600"
+      className="mx-auto rounded-xl justify-center items-center bg-white flex font-medium text-slate-700 hover:text-slate-800 drop-shadow-md hover:drop-shadow-xl transition"
     >
       <span className="flex justify-center items-center ">
         <svg
-          width="16"
-          height="16"
-          viewBox="0 0 12 12"
-          fill="none"
+          version="1.1"
           xmlns="http://www.w3.org/2000/svg"
+          width="18px"
+          height="18px"
+          viewBox="0 0 48 48"
           style={{
-            margin: "16px",
+            margin: "16px 0",
           }}
         >
-          <g clipPath="url(#clip0)">
+          <g>
             <path
-              d="M11.7619 6.13764C11.7619 5.72977 11.7288 5.31971 11.6583 4.91846H5.9989V7.22895H9.23977C9.10529 7.97412 8.67317 8.63332 8.04043 9.05221V10.5514H9.97393C11.1093 9.50637 11.7619 7.9631 11.7619 6.13764Z"
-              fill="#E1E1E6"
-            />
+              fill="#EA4335"
+              d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
+            ></path>
             <path
-              d="M5.99883 12.0002C7.61706 12.0002 8.98175 11.4688 9.97605 10.5517L8.04256 9.05253C7.50462 9.4185 6.81015 9.62574 6.00104 9.62574C4.43573 9.62574 3.10852 8.56971 2.63231 7.1499H0.637085V8.69537C1.65564 10.7215 3.73023 12.0002 5.99883 12.0002Z"
-              fill="#E1E1E6"
-            />
+              fill="#4285F4"
+              d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
+            ></path>
             <path
-              d="M2.63017 7.14986C2.37884 6.40469 2.37884 5.59778 2.63017 4.8526V3.30713H0.637152C-0.213849 5.00252 -0.213849 6.99995 0.637152 8.69533L2.63017 7.14986Z"
-              fill="#E1E1E6"
-            />
+              fill="#FBBC05"
+              d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"
+            ></path>
             <path
-              d="M5.99884 2.37434C6.85425 2.36111 7.681 2.683 8.30051 3.27385L10.0135 1.56082C8.92884 0.542264 7.48919 -0.0177213 5.99884 -8.39483e-05C3.73023 -8.39483e-05 1.65564 1.27862 0.637085 3.30692L2.6301 4.85239C3.10411 3.43038 4.43352 2.37434 5.99884 2.37434Z"
-              fill="#E1E1E6"
-            />
+              fill="#34A853"
+              d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
+            ></path>
+            <path fill="none" d="M0 0h48v48H0z"></path>
           </g>
-          <defs>
-            <clipPath id="clip0">
-              <rect width="11.7619" height="12" fill="white" />
-            </clipPath>
-          </defs>
         </svg>
         <span className="ml-2.5">Sign in with Google</span>
       </span>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,15 @@ import "../styles/build.css";
 import { SessionProvider } from "next-auth/react";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>newtab</title>
+        <meta name="description" content="a new minimal tab" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
